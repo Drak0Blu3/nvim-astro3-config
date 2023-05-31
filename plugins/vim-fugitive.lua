2c81c218ff0737c306816a78c42659b38da91b23
Drak0Blu3 <luisanchezmolina2@gmail.com>
2023-05-31 10:10:11 -0400

    change add key to git options

--- plugins/vim-fugitive.lua
+++ plugins/vim-fugitive.lua
@@ -2,6 +2,6 @@ return {
   "tpope/vim-fugitive",
   lazy = false,
   keys = {
-    { "<leader>gg", "<cmd>G<cr>", desc = "Git options" }, 
+    { "<leader>gg", "<cmd>G<cr>", desc = "Git options" },
   }
 }
