246c1957657d118f2647413310178251ddabe6bb
Drak0Blu3 <luisanchezmolina2@gmail.com>
2023-06-09 14:37:00 -0400

    Add new config to emmet plugin

--- plugins/emmet-vim.lua
+++ plugins/emmet-vim.lua
@@ -1,4 +1,11 @@
 return {
   "mattn/emmet-vim",
-  lazy = true,
+  ft = { "html", "css", "javascript", "javascriptreact", "vue", "typescript", "typescriptreact" },
+  setup = [[require("plugin-config.emmet")]],
+  config = function()
+    vim.g.user_emmet_install_global = 0
+    vim.api.nvim_command(
+      [[autocmd FileType html,css,javascript,javascriptreact,vue,typescript,typescriptreact EmmetInstall]]
+    )
+  end
 }
