2799bc1d9556f3349199286b00847bd48aafd69b
Drak0Blu3 <luisanchezmolina2@gmail.com>
2023-06-09 14:37:54 -0400

    Add keys for move text

---
+++ mappings.lua
@@ -0,0 +1,10 @@
+return {
+  v = {
+    ["K"] = { ":m '>+1<CR>gv=gv", desc = "Move text Down" },
+    ["J"] = { ":m '<-2<CR>gv=gv", desc = "Move text Up" },
+  },
+  n = {
+    ["<leader>k"] = { ":m .+1<CR>==", desc = "Move text Down" },
+    ["<leader>j"] = { ":m .-2<CR>==", desc = "Move text Up" },
+  }
+}
