2007e7324df017b7f0784454ffdbcff621e6e6c9
Drak0Blu3 <luisanchezmolina2@gmail.com>
2023-09-15 23:28:36 -0400

    add nvim-hop plugin for remplace easymotion

---
+++ plugins/nvim-hop.lua
@@ -0,0 +1,12 @@
+return {
+    'smoka7/hop.nvim',
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        {"<leader>m", desc = "Easy Motion" },
+        {"<leader>m2", ":HopChar2<CR>", desc = "Easy Motion Char 2"},
+        {"<leader>mm", ":HopWord<CR>", desc = "Easy Motion Word"},
+        {"<leader>ml", ":HopLine<CR>", desc = "Easy Motion Line"},
+    }
+}
