2dc09c9e4d89404d976782c986cd0170937281c1
Drak0Blu3 <luisanchezmolina2@gmail.com>
2023-11-28 11:21:36 -0400

    add mason ensure installed packages

---
+++ plugins/mason.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "clangd",
+        "cssls",
+        "emmet_ls",
+        "jsonls",
+        "lua_ls",
+        "tsserver",
+      })
+    end,
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "prettierd",
+        "stylua",
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "cpptools",
+      })
+    end,
+  },
+}
